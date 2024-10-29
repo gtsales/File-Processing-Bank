5023f28901375bf9a28317307a23682a058cc8d5
Luiz Sales <soutoluiz0609@gmail.com>
2024-10-29 15:39:30 -0300

    Project created

---
+++ File_Processing_Bank/src/main/java/luiz/sales/file/processing/FileProcessingBankApplication.java
@@ -0,0 +1,13 @@
+package luiz.sales.file.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessingBankApplication.class, args);
+	}
+
+}
---
+++ File_Processing_Bank/src/test/java/luiz/sales/file/processing/FileProcessingBankApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.sales.file.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
